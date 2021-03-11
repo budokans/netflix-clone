7f84f6487590444cebce53973b05a78f63fb5ca6
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 02:31:49 +1300

    Styled accordion and added html-react-parser to
    parse the <br> tags in the faq.body.

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,8 +1,74 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.section``;
-export const Inner = styled.div``;
-export const Item = styled.div``;
-export const Header = styled.div``;
-export const Title = styled.h1``;
-export const Body = styled.p``;
+export const Container = styled.section`
+  border-bottom: 8px solid #222;
+`;
+
+export const Inner = styled.div`
+  display: flex;
+  padding: 70px 45px;
+  flex-direction: column;
+  max-width: 815px;
+  margin: auto;
+
+  @media (max-width: 600px) {
+    padding: 60px 0;
+  }
+`;
+
+export const Item = styled.div`
+  color: white;
+  margin-bottom: 10px;
+
+  &:first-of-type {
+    margin-top: 3em;
+  }
+
+  @media (max-width: 600px) {
+    &:first-of-type {
+      margin-top: 1em;
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-top: 0;
+  margin-bottom: 8px;
+  color: white;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 26px;
+  }
+`;
+
+export const Header = styled.div`
+  cursor: pointer;
+  margin-bottom: 1px;
+  font-size: 26px;
+  background: #303030;
+  padding: 1.2em;
+  user-select: none;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
+
+export const Body = styled.p`
+  max-width: 1200px;
+  font-size: 26px;
+  font-weight: normal;
+  line-height: 1.35;
+  background: #303030;
+  padding: 1.2em;
+  user-select: none;
+  margin: 0;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+    line-height: 1.3;
+  }
+`;
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Accordion } from "../components";
 import faqsData from "../fixtures/faqs.json";
+import parse from "html-react-parser";
 
-export function FAQsContainer({ children, restProps }) {
+export function FAQsContainer() {
   return (
     <Accordion>
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqsData.map((faq) => {
-        return (
-          <Accordion.Item key={faq.id}>
-            <Accordion.Header>{faq.header}</Accordion.Header>
-            <Accordion.Body>{faq.body}</Accordion.Body>
-          </Accordion.Item>
-        );
-      })}
+      {faqsData.map((faq) => (
+        <Accordion.Item key={faq.id}>
+          <Accordion.Header>{faq.header}</Accordion.Header>
+          <Accordion.Body>{parse(faq.body)}</Accordion.Body>
+        </Accordion.Item>
+      ))}
     </Accordion>
   );
 }
