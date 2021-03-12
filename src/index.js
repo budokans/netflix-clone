4cf4c1dd5d67ac0b27ee707a7c6046d11be41e03
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 16:31:58 +1300

    Imported normalize.css!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
+import "normalize.css";
 import { GlobalStyles } from "./global-styles";
 
 ReactDOM.render(
