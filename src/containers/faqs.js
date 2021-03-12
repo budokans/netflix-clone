87ee0d99e1e0d359822b6dcf43b920ebea15c84a
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 16:25:46 +1300

    Built out OptForm skeleton and imported to FAQsContainer

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 import faqsData from "../fixtures/faqs.json";
 import parse from "html-react-parser";
 
 export function FAQsContainer() {
   return (
-    <Accordion>
-      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqsData.map((faq) => (
-        <Accordion.Item key={faq.id}>
-          <Accordion.Header>{faq.header}</Accordion.Header>
-          <Accordion.Body>{parse(faq.body)}</Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
+    <>
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        {faqsData.map((faq) => (
+          <Accordion.Item key={faq.id}>
+            <Accordion.Header>{faq.header}</Accordion.Header>
+            <Accordion.Body>{parse(faq.body)}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+        </OptForm>
+      </Accordion>
+    </>
   );
 }
