48c813c1bcca0c1d2ebb4a9d7805314b90993dcc
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 16:58:58 +1300

    Routes constant file added and imported in app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import Home from "./pages/home";
 
 export function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/signin">
+        <Route path={ROUTES.SIGN_IN}>
           <p>I will be the sign in page</p>
         </Route>
-        <Route path="/signup">
+        <Route path={ROUTES.SIGN_UP}>
           <p>I will be the sign up page</p>
         </Route>
-        <Route path="/browse">
+        <Route path={ROUTES.BROWSE}>
           <p>I will be the browse page</p>
         </Route>
-        <Route path="/">
+        <Route path={ROUTES.HOME}>
           <Home />
         </Route>
       </Switch>
