6cf1903be47b60054b326f40622966f491d88874
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 19:09:27 +1300

    Styled Header component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,16 +1,65 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import { Link as ReachRouterLink } from "react-router-dom";
 
 export const Background = styled.section`
   display: flex;
   flex-direction: column;
+  background: url(${({ src }) =>
+      src ? `../images/misc/${src}.jpg` : "../images/misc/home-bg.jpg"})
+    top left / cover no-repeat;
+
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) =>
+      dontShowOnSmallViewPort && `background: none`}
+  }
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
 
-export const Link = styled.p``;
+  a {
+    display: flex;
+  }
 
-export const ButtonLink = styled(ReachRouterLink)``;
+  @media (max-width: 1000px) {
+    margin: 0 30px;
+  }
+`;
+
+export const Link = styled.p`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active ? "700" : "normal")};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+export const ButtonLink = styled(ReachRouterLink)`
+  display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
+`;
 
 export const Logo = styled.img`
   height: 32px;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { OptForm } from "../components";
 import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FAQsContainer } from "../containers/faqs";
@@ -7,7 +8,16 @@ import { FooterContainer } from "../containers/footer";
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Text>
+            Ready to watch Netflix? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Get started</OptForm.Button>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FAQsContainer />
       <FooterContainer />
