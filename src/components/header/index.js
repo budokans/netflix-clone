22e3ac8824b2b059610fd9468666d19b2d38e97e
budokans <68995729+budokans@users.noreply.github.com>
2021-03-12 17:51:56 +1300

    Built out Header styled component
    and container!

---
+++ src/components/header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link as ReachRouterLink } from "react-router-dom";
+import { Background, Container, Logo, ButtonLink } from "./styles/header";
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Logo = function HeaderLogo({ path, ...restProps }) {
+  return (
+    <ReachRouterLink to={path}>
+      <Logo {...restProps} />
+    </ReachRouterLink>
+  );
+};
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FAQsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
@@ -6,6 +7,7 @@ import { FooterContainer } from "../containers/footer";
 export default function Home() {
   return (
     <>
+      <HeaderContainer />
       <JumbotronContainer />
       <FAQsContainer />
       <FooterContainer />
