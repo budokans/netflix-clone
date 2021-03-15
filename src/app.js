18eacaf3a064a06777dee07c5323678d3a8592c2
budokans <68995729+budokans@users.noreply.github.com>
2021-03-16 02:25:50 +1300

    Add pages library for tree-shaking
    and reimported from app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import Home from "./pages/home";
-import SignIn from "./pages/signin";
+import { Home, SignIn } from "./pages";
 
 export function App() {
   return (
