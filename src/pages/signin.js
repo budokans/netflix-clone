b41b2ada95042ba263b92bb5168f6909533b07f2
budokans <68995729+budokans@users.noreply.github.com>
2021-03-16 02:40:31 +1300

    Added the rest of the form elements
    also added:
    
    1. State for the email and password inputcontrolled components
    2. a TBC handleSignIn().

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,12 +4,48 @@ import { HeaderContainer } from "../containers/header";
 
 export default function SignIn() {
   const [error, setError] = useState(false);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn(e) {
+    e.preventDefault();
+
+    // Call firebase for validation
+  }
 
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign in</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignIn} method="POST">
+          <Form.Input
+            type="email"
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={(e) => setEmailAddress(e.target.value)}
+          />
+
+          <Form.Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            autoComplete="off"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Form.Submit disabled={false} type="submit">
+            Sign in
+          </Form.Submit>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot.
+          </Form.TextSmall>
+        </Form.Base>
       </Form>
     </HeaderContainer>
   );
