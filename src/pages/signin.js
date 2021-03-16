c5f41bf2057590a4cebe32bd708637359cc34951
budokans <68995729+budokans@users.noreply.github.com>
2021-03-16 18:59:45 +1300

    Added footer to SignIn page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form } from "../components";
 import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
 
 export default function SignIn() {
   const [error, setError] = useState(false);
@@ -16,42 +17,46 @@ export default function SignIn() {
   }
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign in</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign in</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base onSubmit={handleSignIn} method="POST">
-          <Form.Input
-            type="email"
-            placeholder="Email address"
-            value={emailAddress}
-            onChange={(e) => setEmailAddress(e.target.value)}
-            autoComplete="email"
-            required
-          />
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              type="email"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={(e) => setEmailAddress(e.target.value)}
+              autoComplete="email"
+              required
+            />
 
-          <Form.Input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="password"
-            required
-          />
-          <Form.Submit disabled={isInvalid} type="submit">
-            Sign in
-          </Form.Submit>
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="password"
+              required
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign in
+            </Form.Submit>
 
-          <Form.Text>
-            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA to ensure you're not a
-            bot.
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+            <Form.Text>
+              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+
+      <FooterContainer />
+    </>
   );
 }
