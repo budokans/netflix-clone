f28372fbc6aa9f5490a2d0bc538150b97088c876
budokans <68995729+budokans@users.noreply.github.com>
2021-03-16 22:37:41 +1300

    Completed SignUp page skeleton
    Basic state (controlled form components) and validation done.

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,13 +2,68 @@ import React, { useState } from "react";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 import { Form } from "../components";
-import * as ROUTES from "../constants/routes";
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = firstName === "" || emailAddress === "" || password === "";
+
+  function handleSignUp(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <HeaderContainer />
-      <Form></Form>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              autoComplete="given-name"
+              required
+            />
+
+            <Form.Input
+              type="email"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={(e) => setEmailAddress(e.target.value)}
+              autoComplete="email"
+              required
+            />
+
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="password"
+              required
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign up
+            </Form.Submit>
+
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+
       <FooterContainer />
     </>
   );
