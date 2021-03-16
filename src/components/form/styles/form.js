665b822c8c3b6ed3addd38e4ba932a28f317b619
budokans <68995729+budokans@users.noreply.github.com>
2021-03-16 18:42:25 +1300

    Styled form component

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,12 +1,88 @@
 import styled from "styled-components/macro";
 import { Link as ReachRouterLink } from "react-router-dom";
 
-export const Container = styled.section``;
-export const Base = styled.form``;
-export const Title = styled.h1``;
-export const Text = styled.p``;
-export const TextSmall = styled.small``;
-export const Link = styled(ReachRouterLink)``;
-export const Input = styled.input``;
-export const Submit = styled.button``;
-export const Error = styled.div``;
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  min-height: 660px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto;
+  max-width: 450px;
+  padding: 60px 68px 40px;
+  margin-bottom: 100px;
+`;
+
+export const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 28px;
+`;
+
+export const Text = styled.p`
+  color: #8c8c8c;
+  font-size: 16px;
+  margin-bottom: 28px;
+`;
+
+export const TextSmall = styled.small`
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
+`;
+
+export const Link = styled(ReachRouterLink)`
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Input = styled.input`
+  background: #333;
+  border-radius: 4px;
+  border: 0;
+  color: #fff;
+  height: 40px;
+  line-height: 40px;
+  padding: 5px 20px;
+  margin-bottom: 20px;
+
+  &:last-of-type {
+    margin-bottom: 30px;
+  }
+`;
+
+export const Submit = styled.button`
+  background: #e50914;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 12px;
+  padding: 16px;
+  border: 0;
+  color: white;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`;
+
+export const Error = styled.div`
+  background: #e87c03;
+  border-radius: 4px;
+`;
