18c41b434d759ae649c5613af0cc4204b0ca17f8
budokans <68995729+budokans@users.noreply.github.com>
2021-03-17 21:40:57 +1300

    Built selectionMap function for ordering data

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
+import { selectionMap } from "../utils";
 
 export default function Browse() {
-  const { films } = useContent("films");
   const { series } = useContent("series");
+  const { films } = useContent("films");
+  const slides = selectionMap({ series, films });
 
-  return <BrowseContainer />;
+  console.log(slides);
+
+  return <BrowseContainer slides={slides} />;
 }
