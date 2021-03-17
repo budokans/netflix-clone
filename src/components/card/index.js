40d6f7ef4712361765bf39e36a9b146530340b14
budokans <68995729+budokans@users.noreply.github.com>
2021-03-18 00:19:48 +1300

    Built Card compound component
    Creates context in which to hold information about if a feature is open and which item's feature it is.
    
    Only the card holding the item to be featured (itemFeature) can be showing a feature (showFeature).

---
+++ src/components/card/index.js
@@ -0,0 +1,107 @@
+import React, { useState, useContext, createContext } from "react";
+import {
+  Container,
+  Group,
+  Title,
+  Subtitle,
+  Text,
+  Entities,
+  Meta,
+  Item,
+  Image,
+  Feature,
+  Content,
+  FeatureTitle,
+  FeatureClose,
+  Maturity,
+  FeatureText,
+} from "./styles/card";
+
+const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState(false);
+
+  return (
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children}</Container>
+    </FeatureContext.Provider>
+  );
+}
+
+Card.Group = function CardGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Card.Subtitle = function CardSubtitle({ children, ...restProps }) {
+  return <Subtitle {...restProps}>{children}</Subtitle>;
+};
+
+Card.Text = function CardText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+  return <Entities {...restProps}>{children}</Entities>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+  return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Item = function CardItem({ item, children, ...restProps }) {
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
+  return (
+    <Item
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+      {...restProps}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
+
+Card.Feature = function CardFeature({ category, children, ...restProps }) {
+  const { itemFeature, showFeature, setShowFeature } = useContext(
+    FeatureContext
+  );
+
+  return showFeature ? (
+    <Feature
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature.title}</FeatureTitle>
+        <FeatureText>{itemFeature.description}</FeatureText>
+        <FeatureClose onClick={() => setShowFeature(false)}>
+          <img src="/images/icons/close.png" alt="Close" />
+        </FeatureClose>
+
+        <Group margin="30px 0" flexDirection="row" alignItems="center">
+          <Maturity rating={itemFeature.maturity}>
+            {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}{" "}
+          </Maturity>
+          <FeatureText fontWeight="bold">
+            {itemFeature.genre.charAt(0).toUpperCase() +
+              itemFeature.genre.slice(1)}
+          </FeatureText>
+        </Group>
+      </Content>
+    </Feature>
+  ) : null;
+};
---
+++ src/components/card/styles/card.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div``;
+export const Group = styled.div``;
+export const Title = styled.h1``;
+export const Subtitle = styled.h2``;
+export const Text = styled.p``;
+export const Entities = styled.div``;
+export const Meta = styled.div``;
+export const Item = styled.div``;
+export const Image = styled.img``;
+export const Feature = styled.div``;
+export const Content = styled.div``;
+export const FeatureTitle = styled.h2``;
+export const FeatureClose = styled.button``;
+export const Maturity = styled.h3``;
+export const FeatureText = styled.p``;
