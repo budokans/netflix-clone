333ee63fd7367b06cdae12460a9d5dceccbbb6a9
budokans <68995729+budokans@users.noreply.github.com>
2021-03-17 14:18:16 +1300

    Styled new Header search subcomponents

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -124,6 +124,35 @@ export const PlayButton = styled.button`
     }
 `;
 
-export const Search = styled.div``;
-export const SearchInput = styled.input``;
-export const SearchIcon = styled.button``;
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SearchIcon = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 16px;
+  }
+`;
+
+export const SearchInput = styled.input`
+  background-color: #44444459;
+  color: white;
+  border: 1px solid white;
+  transition: width 0.3s;
+  height: 30px;
+  font-size: 14px;
+  margin-left: ${({ active }) => (active ? "10px" : 0)};
+  padding: ${({ active }) => (active === true ? "0 10px" : "0")};
+  opacity: ${({ active }) => (active === true ? "1" : "0")};
+  width: ${({ active }) => (active === true ? "200px" : "0px")};
+`;
