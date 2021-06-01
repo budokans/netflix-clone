7b22c75604e46a53581e437a9214ebb91c91a8b9
budokans <68995729+budokans@users.noreply.github.com>
2021-06-01 16:20:07 +1200

    Test Home page

--- src/__tests__/pages/home.test.js
+++ src/__tests__/pages/home.test.js
@@ -5,5 +5,15 @@ import { Home } from "../../pages";
 jest.mock("react-router-dom");
 
 test("renders the Home page", () => {
-  const { getByText, getByTestId } = render(<Home />);
+  const { getByText, getAllByText, getAllByPlaceholderText } = render(<Home />);
+
+  expect(getByText("Unlimited movies, TV shows and more.")).toBeTruthy();
+  expect(getByText("Watch anywhere. Cancel at any time.")).toBeTruthy();
+  expect(
+    getAllByText(
+      "Ready to watch Netflix? Enter your email to create or restart your membership."
+    )
+  ).toBeTruthy();
+  expect(getAllByText("Get started")).toBeTruthy();
+  expect(getAllByPlaceholderText("Email address")).toBeTruthy();
 });
